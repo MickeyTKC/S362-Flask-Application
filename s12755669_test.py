90f1b93fef930e7959cee2b82b2b4122813acd79
MickeyTKC <tt51145505@gmail.com>
2024-11-23 02:15:56 +0800

    Update s12755669_test.py

--- s12755669_test.py
+++ s12755669_test.py
@@ -10,17 +10,57 @@ class TestFlask(unittest.TestCase):
         req = {"username":1234, "password":"s1234-pw", "simulations":0, "concurrency":1}
         res = requests.post("http://localhost:5000/pi", json=req)
         print(res.json())
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 401)
         req = {"username":"s1234", "password":"s1234-pw", "simulations":0, "concurrency":1}
         res = requests.post("http://localhost:5000/pi", json=req)
         print(res.json())
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 401)
+    """
+    
     """
     def test_pi(self):
-        req = {"username":"1234", "password":"1234-pw", "simulations":10000000, "concurrency":8}
+        #success
+        req = {"username":"1234", "password":"1234-pw", "simulations":100, "concurrency":1}
         res = requests.post("http://localhost:5000/pi", json=req)
         print(res.json())
-        #self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 200)
+        #success
+        req = {"username":"1234", "password":"1234-pw", "simulations":100000000, "concurrency":8}
+        res = requests.post("http://localhost:5000/pi", json=req)
+        print(res.json())
+        self.assertEqual(res.status_code, 200)
+        #invlid simulations
+        req = {"username":"1234", "password":"1234-pw", "simulations":0, "concurrency":8}
+        res = requests.post("http://localhost:5000/pi", json=req)
+        print(res.json())
+        self.assertEqual(res.status_code, 400)
+        #success concurrency
+        req = {"username":"1234", "password":"1234-pw", "simulations":100000000, "concurrency":0}
+        res = requests.post("http://localhost:5000/pi", json=req)
+        print(res.json())
+        self.assertEqual(res.status_code, 400)
+    """
+    def test_legacy_pi(self):
+        #success tcp
+        req = {"username":"1234", "password":"1234-pw", "protocol":"tcp", "concurrency":1}
+        res = requests.post("http://localhost:5000/legacy_pi", json=req)
+        print(res.json())
+        self.assertEqual(res.status_code, 200)
+        #success udp
+        req = {"username":"1234", "password":"1234-pw", "protocol":"udp", "concurrency":1}
+        res = requests.post("http://localhost:5000/legacy_pi", json=req)
+        print(res.json())
+        self.assertEqual(res.status_code, 200)
+        #success tcp concurrency 8
+        req = {"username":"1234", "password":"1234-pw", "protocol":"tcp", "concurrency":8}
+        res = requests.post("http://localhost:5000/legacy_pi", json=req)
+        print(res.json())
+        self.assertEqual(res.status_code, 200)
+        #success udp concurrency 8
+        req = {"username":"1234", "password":"1234-pw", "protocol":"udp", "concurrency":8}
+        res = requests.post("http://localhost:5000/legacy_pi", json=req)
+        print(res.json())
+        self.assertEqual(res.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
