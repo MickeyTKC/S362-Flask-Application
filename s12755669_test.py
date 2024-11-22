5e9594dde8d22c1be36d69127aebe905f6c2c835
MickeyTKC <tt51145505@gmail.com>
2024-11-22 20:31:02 +0800

    Testing R1 with correct case

--- s12755669_test.py
+++ s12755669_test.py
@@ -5,7 +5,7 @@ import json, requests
 
 
 class TestFlask(unittest.TestCase):
-    
+    """
     def test_failed_login(self):
         req = {"username":1234, "password":"s1234-pw", "simulations":0, "concurrency":1}
         res = requests.post("http://localhost:5000/pi", json=req)
@@ -15,9 +15,9 @@ class TestFlask(unittest.TestCase):
         res = requests.post("http://localhost:5000/pi", json=req)
         print(res.json())
         self.assertEqual(res.status_code, 200)
-
+    """
     def test_pi(self):
-        req = {"username":"1234", "password":"1234-pw", "simulations":0, "concurrency":1}
+        req = {"username":"1234", "password":"1234-pw", "simulations":10000000, "concurrency":8}
         res = requests.post("http://localhost:5000/pi", json=req)
         print(res.json())
         #self.assertEqual(res.status_code, 200)
