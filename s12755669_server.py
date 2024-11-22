54c2dcb8520f34ff416d8e208c157cacd78c4e58
MickeyTKC <tt51145505@gmail.com>
2024-11-22 20:28:31 +0800

    R1

--- s12755669_server.py
+++ s12755669_server.py
@@ -1,14 +1,18 @@
 #server side flask application
 import os, time
 from flask import Flask, request, jsonify
-import random
+from random import random
+from concurrent.futures import ProcessPoolExecutor
 
+#config
 app = Flask(__name__)
 
 #localhost and port:5000
 HOST, PORT = "0.0.0.0", 5000
 
 app = Flask(__name__)
+
+#functions
 
 #R4
 def login(username, password):
@@ -19,11 +23,24 @@ def login(username, password):
     if type(password) != str: return False
     if password != f"{username}-pw": return False
     return True
+
+#R1 
+def compute_count(simulations):
+    count = 0
+    for _ in range(simulations):
+        x = random()
+        y = random()
+        if x * x + y * y < 1:
+            count += 1
+    return count
     
-#Testing    
+
+#routing   
+    
+#testing flask server   
 @app.get("/")
 def index():
-    return jsonify({"message": "Hello, World!"})
+    return jsonify({"application": "PI Calculator"})
 
 #R1
 @app.post("/pi")
@@ -39,11 +56,16 @@ def pi():
     concurrency = get_json.get("concurrency")
     #process part of the service
     start = time.time() #start execturion time
-    pi = 3.14
-    time.sleep(1)
+    if concurrency > 1:
+        with ProcessPoolExecutor(max_workers=concurrency) as executor:
+            futures = [executor.submit(compute_count, simulations // concurrency) for _ in range(concurrency)]
+            total_count = sum(future.result() for future in futures)
+    else:
+        total_count = compute_count(simulations)
+    pi_estimate = total_count / simulations * 4
     end = time.time() #end execturion time
     #send response
-    return jsonify({"simulations": simulations, "concurrency": concurrency, "pi": pi, "execution_time": end-start})
+    return jsonify({"simulations": simulations, "concurrency": concurrency, "pi": pi_estimate, "execution_time": end-start})
 
 #R2
 @app.post("/legacy_pi")
